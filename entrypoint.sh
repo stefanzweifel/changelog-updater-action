3b54ec66922355614bf0b80b290d6634a315acf5
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2023-05-29 14:50:30 +0200

    Add `hide-release-date` option (#33)
    * Update CLI
    
    * Add hide-release-date option
    
    * Update entrypoint.sh
    
    * Change default value

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ php /changelog-updater update \
 --path-to-changelog="$4" \
 --compare-url-target-revision="$5" \
 --heading-text="$6" \
+$( [ "$7" ] && echo "--hide-release-date" ) \
 --github-actions-output \
 --write \
 --no-interaction
